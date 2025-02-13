848e9b7332d0d526d3e9c66790fd12f0915c0e67
cathrinj08 <cathrinjoe007@gmail.com>
2025-02-13 15:36:37 +0530

    Add files via upload

---
+++ patterndiamond1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class patterndiamond1{
+    public static void main(String[] args){
+        int n=5;
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=n-i;j>=1;j--){
+               System.out.print(" ");
+           } for(int k=1;k<=i;k++) 
+            if (k == 1 || k == i) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+          
+           System.out.println();
+       }
+       
+        for(int i=n-1;i>=1;i--)
+       {
+           for(int j=n-i;j>=1;j--){
+               System.out.print(" ");
+           } for(int k=1;k<=i;k++)
+           if (k == 1 || k == i) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+           System.out.println();
+       }
+    }
+    
+}
+
+    

1ece962beeb9349855d448b2762c7c5c4acecda3
cathrinj08 <cathrinjoe007@gmail.com>
2025-02-13 12:06:13 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*****************************************************************************/
+import java.util.*;
+public class pattern{
+    public static void main(String[] args){
+        int n=5;
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=1;j<=i;j++)
+           {
+               System.out.print("1" );
+           }
+           System.out.println();
+       }
+    }
+    
+}
+        
